b7eb03fb815d15d9853fcea3f3198a49332b08c7
Anthony Muratore <amurato1@binghamton.edu>
2017-10-31 16:29:48 -0400

    small changes to design

--- node.cpp
+++ node.cpp
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <fstream>
+#include <string>
 
 using namespace std;
+
+class node
+{
+	public:
+		
+	private: //maybe just make them all public?
+		int nodeid, ctrlPort, dataPort;
+		string hostName;
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -29,6 +40,15 @@ int main(int argc, char *argv[])
 		cerr<<"Unable to open file "<<argv[1]<<endl;
 		exit(1);
 	}
+	string line;
+	while(getline(inFile, line))
+	{
+		//instantiate a class first?
+		istringstream in(line);
+		in>>nodeID>>hostName>>ctrlPort>>dataPort;
+		//need to handle some random number of neighbors after
+	}
+	inFile.close();
 	
 	
 	/*sockfd = socket(AF_INET, SOCK_DGRAM, 0);
